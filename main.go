5e4438af4ae04f3d318cc55cc2764a33c5271a71
Lucas Renan <contato@lucasrenan.com>
2018-10-22 09:56:28 +0200

    Sum function

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+func sum(start int, end int) int {
+	total := 0
+	for i := total; i <= end; i++ {
+		total += 1
+	}
+
+	return total
+}
+
 func main() {
 	fmt.Println("hello world")
+
+	fmt.Println("sum(1, 10) = %d\n", sum(1, 10))
 }
