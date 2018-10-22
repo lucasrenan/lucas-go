0d813d1ecd4f9da526f0d4fd7b84e7b18caf6bce
Lucas Renan <contato@lucasrenan.com>
2018-10-22 10:06:47 +0200

    Add fibonacci func

--- main.go
+++ main.go
@@ -13,8 +13,18 @@ func sum(start int, end int) int {
 	return total
 }
 
+func fib(n int) int {
+	a, b := 1, 1
+	for n > 0 {
+		a, b = b, a+b
+		n--
+	}
+
+	return a
+}
+
 func main() {
 	fmt.Println("hello world")
-
 	fmt.Println("sum(1, 10) = %d\n", sum(1, 10))
+	fmt.Println("fib(10) = %d\n", fib(10))
 }
