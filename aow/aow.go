4c2c3298a8ce84233c7597a0b7d55f5082c31bc1
Lucas Renan <contato@lucasrenan.com>
2018-10-22 11:51:43 +0200

    max used work on aow.txt

--- aow/aow.go
+++ aow/aow.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("hello aow")
+	file, err := os.Open("aow.txt")
+	if err != nil {
+		log.Fatalf("error: %s\n", err)
+	}
+
+	counts := map[string]int{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		words := strings.Fields(scanner.Text())
+		for _, word := range words {
+			counts[strings.ToLower(word)]++
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("error: can't scan - %s", err)
+	}
+
+	maxWord, maxCount := "", 0
+	for word, count := range counts {
+		if count > maxCount {
+			maxWord, maxCount = word, count
+		}
+	}
+
+	fmt.Printf("%s (%d)\n", maxWord, maxCount)
 }
