499c371b9156bf95084f8afab72c4df1da91649a
Lucas Renan <contato@lucasrenan.com>
2018-10-22 10:20:51 +0200

    for loops

--- main.go
+++ main.go
@@ -27,4 +27,21 @@ func main() {
 	fmt.Println("hello world")
 	fmt.Println("sum(1, 10) = %d\n", sum(1, 10))
 	fmt.Println("fib(10) = %d\n", fib(10))
+
+	s := "Hello"
+	for i := range s {
+		fmt.Println(i)
+	}
+
+	for i, c := range s {
+		fmt.Println("%d: %c\n", i, c)
+	}
+
+	for i, c := range s[1:3] {
+		fmt.Println("%d: %c\n", i, c)
+	}
+
+	for _, c := range s {
+		fmt.Println(c)
+	}
 }
