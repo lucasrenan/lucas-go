e4edf2756830e18447526ddc9deec4db8604aac3
Lucas Renan <contato@lucasrenan.com>
2018-10-23 15:17:31 +0200

    mod example

---
+++ travel/travel.go
@@ -0,0 +1,17 @@
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/spf13/cobra"
+// )
+
+// func main() {
+// 	cmd := &cobra.Command{
+// 		Use: 	"travel",
+// 		Short: 	"Some command",
+// 		Run: 	func(cmd *cobra.Command, args []string) {
+
+// 		}
+// 	}
+// }
